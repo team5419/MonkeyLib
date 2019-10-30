e8294b57b06f7fb87723137fbd6a36b5378c9bfe
Nicholas Underwood <nachos.underwood@gmail.com>
2019-10-30 09:00:30 -0700

    fixed errors

--- src/main/kotlin/org/team5419/fault/math/splines/InterlopedCubicBezierSplines.kt
+++ src/main/kotlin/org/team5419/fault/math/splines/InterlopedCubicBezierSplines.kt
@@ -2,7 +2,7 @@ package org.team5419.fault.math.splines
 
 import org.team5419.fault.math.splines.Spline
 import org.team5419.fault.math.splines.CubicBezierSpline
-import rg.team5419.fault.geometry.Vector2
+import org.team5419.fault.geometry.Vector2
 import org.team5419.fault.geometry.Rotation2d
 
 public class InterlopedCubicBezierSplines: Spline(vararg splines: CubicBezierSpline) {
@@ -12,13 +12,14 @@ public class InterlopedCubicBezierSplines: Spline(vararg splines: CubicBezierSpl
 
     init{
         this.splines = splines
+        //TODO confirm interlopation
     }
 
     private fun getSpline(t: Double): CubicBezierSpline = this.splines.get( (t * this.numSplines).toInt() )
     private fun getRelativeT(t: Double): Double = (t * this.numSplines) - (t * this.numSplines).toInt()
 
     override public fun getPoint(t: Double): Vector2 = getSpline(t).getPoint(getRelativeT(t))
-    override public fun getHeading(t: Double): Vector2 = getSpline(t).getHeading(getRelativeT(t))
-    override public fun getCurvature(t: Double): Vector2 = getSpline(t).getCurvature(getRelativeT(t))
-
+    override public fun getHeading(t: Double): Rotation2d = getSpline(t).getHeading(getRelativeT(t))
+    override public fun getCurvature(t: Double): Double = getSpline(t).getCurvature(getRelativeT(t))
+    override public fun getDCurvature(t: Double): Double = getSpline(t).getDCurvature(getRelativeT(t))
 }
