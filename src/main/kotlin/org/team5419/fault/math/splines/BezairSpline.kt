5a95c085853c347bf8208bb0ae6f901c90077bb3
Nicholas <nachos.underwood@gmail.com>
2019-10-29 17:06:40 -0700

    done?

--- src/main/kotlin/org/team5419/fault/math/splines/BezairSpline.kt
+++ src/main/kotlin/org/team5419/fault/math/splines/BezairSpline.kt
@@ -3,53 +3,38 @@ package org.team5419.fault.math.splines
 import org.team5419.fault.math.geometry.Vector2
 import org.team5419.fault.math.geometry.Rotation2d
 import org.team5419.fault.math.geometry.Pose2d
-package org.team5419.fault.math.splines.Spline
 
-
-class BerzairCubicSpline(
-    
-) Spline {
-
-    private val ax: Double
-    private val bx: Double
-    private val cx: Double
-    private val dx: Double
-
-    private val ay: Double
-    private val by: Double
-    private val cy: Double
-    private val dy: Double
+class BerzairCubicSpline(p0: Vector2, p1: Vector2, p2: Vector2, p3: Vector2) : Spline() {
+    private val p0: Vector2
+    private val p1: Vector2
+    private val p2: Vector2
+    private val p3: Vector2
 
     init{
-
+        this.p0 = p0
+        this.p1 = p1
+        this.p2 = p2
+        this.p3 = p3
     }
 
-    override public fun getPoint(t): Vector2 {
-        val endIndex = getAnchors() - ceil(t)
-        val d = points.get(endIndex)
-        val c = points.get(endIndex - 1)
-        val b = points.get(endIndex - 2)
-        val a = points.get(endIndex - 3)
-        return cerp(a,b,c,d,t)
+    // B(t) = (1 - t)^3P0 + 3(1 - t)^2tP1 + 3(1 - t)t^2P2 + t^3P3
+    override public fun getPoint(t: Double): Vector2 {
+        val px = Math.pow(1-t,3.0) * p0.x + 3 * Math.pow(1-t,2.0) * t * p1.x + 3 * (1-t) * Math.pow(t,2.0) * p2.x + Math.pow(t,3.0) * p3.x
+        val py = Math.pow(1-t,3.0) * p0.y + 3 * Math.pow(1-t,2.0) * t * p1.y + 3 * (1-t) * Math.pow(t,2.0) * p2.y + Math.pow(t,3.0) * p3.y
+        return Vector2(px, py)
     }
 
-    override public fun getCurvature(t): Double {
-        Math.sqrt()
+    // B'(t) = 3(1- t)^2(P1 - P0) + 6(1 - t)t(P2 - P1) + 3t^2(P3 - P2)
+    override public fun getHeading(t: Double): Rotation2d {
+        val px = (1-t) * (1-t) * (p1.x - p0.x) + 2 * (1-t) * t * (p2.x - p1.x) + t * t * (p3.x - p2.x)
+        val py = (1-t) * (1-t) * (p1.y - p0.y) + 2 * (1-t) * t * (p2.y - p1.y) + t * t * (p3.y - p2.y)
+        return Rotation2d(px, py, true)
     }
 
+    override fun getCurvature(t: Double): Double = 0.0
 
-    //get direction radian 
-    override fun getHeadingAtWaypoint(t: Double): Rotation2d {
-        val wayPointIndex = getWaypointIndex(t)
-        val p0 = points.get(wayPointIndex)
-        val p1 = points.get(wayPointIndex - 1) 
-        return Math.atan( (p1.y - p0.y) / (p1.x - p0.x) )
-    }
+    override fun getDCurvature(t: Double) = 0.0
 
-    override fun getCurvature(t: Double): Double
-
-    override fun getDCurvature(t: Double): Double
-
-    override fun getVelocity(t: Double): Double
+    override fun getVelocity(t: Double) = 0.0
 
 }
