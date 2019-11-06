81fc1150fb3f7812c4860026bc5bee34bb833574
John McAllister <theepicpenguin121@gmail.com>
2019-11-05 18:45:42 -0800

    LazyTalon + LazyVictor Documentation Completed

--- src/main/kotlin/org/team5419/fault/hardware/LazyVictorSPX.kt
+++ src/main/kotlin/org/team5419/fault/hardware/LazyVictorSPX.kt
@@ -5,6 +5,10 @@ import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.DemandType
 import com.ctre.phoenix.motorcontrol.NeutralMode
 
+/**
+ * Like a Victor, except it stops you from repeating commands.
+ * @param deviceNumber The id of the Victor.
+ */
 class LazyVictorSPX(deviceNumber: Int) : VictorSPX(deviceNumber) {
 
     private var lastSet = Double.NaN
@@ -13,6 +17,11 @@ class LazyVictorSPX(deviceNumber: Int) : VictorSPX(deviceNumber) {
     private var lastDemandType: DemandType? = null
     private var lastBrakeMode: NeutralMode? = null
 
+    /**
+     * Sets the speed of the motor based on the mode and value.
+     * @param mode Tells the controller how to interpret the value supplied.
+     * @param value The value of the motor based on the mode set.
+     */
     public override fun set(mode: ControlMode, value: Double) {
         if (value != lastSet || mode != lastControlMode) {
             lastSet = value
@@ -23,6 +32,14 @@ class LazyVictorSPX(deviceNumber: Int) : VictorSPX(deviceNumber) {
         }
     }
 
+    /**
+     * Sets the speed of the motor based on the modes and values.
+     * For further reading: https://tinyurl.com/y4chch5z
+     * @param mode Tells the controller how to interpret the values supplied. See further reading.
+     * @param value0 The value of the motor based on the mode set.
+     * @param demandType Tells the controller how to affect the values supplied. See further reading.
+     * @param value1 The value of which the motor is affected based on the demandType.
+     */
     public override fun set(mode: ControlMode, value0: Double, demandType: DemandType, value1: Double) {
         @Suppress("ComplexCondition")
         if (value0 != lastSet || mode != lastControlMode ||
@@ -35,6 +52,10 @@ class LazyVictorSPX(deviceNumber: Int) : VictorSPX(deviceNumber) {
         }
     }
 
+    /**
+     * Sets the mode of operation during neutral throttle output.
+     * @param brakeMode Tells the controller to either brake or coast.
+     */
     public override fun setNeutralMode(brakeMode: NeutralMode) {
         if (lastBrakeMode != brakeMode) {
             lastBrakeMode = brakeMode
