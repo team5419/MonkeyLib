f04f14df43d646b03982e61fc6684c0dbed9e806
Nicholas <nachos.underwood@gmail.com>
2019-12-29 20:46:44 -0800

    need velocity and unit tests

--- src/main/kotlin/org/team5419/fault/math/splines/CubicBezierSpline.kt
+++ src/main/kotlin/org/team5419/fault/math/splines/CubicBezierSpline.kt
@@ -1,13 +1,13 @@
 package org.team5419.fault.math.splines
 
-import org.team5419.fault.math.geometry.Vector2
+import org.team5419.fault.math.geometry.Vector2d
 import org.team5419.fault.math.geometry.Rotation2d
 
-class CubicBezierSpline(p0: Vector2, p1: Vector2, p2: Vector2, p3: Vector2) : Spline() {
-    public var p0: Vector2
-    public var p1: Vector2
-    public var p2: Vector2
-    public var p3: Vector2
+class CubicBezierSpline(p0: Vector2d, p1: Vector2d, p2: Vector2d, p3: Vector2d) : Spline() {
+    public var p0: Vector2d
+    public var p1: Vector2d
+    public var p2: Vector2d
+    public var p3: Vector2d
 
     init {
         this.p0 = p0
@@ -17,30 +17,30 @@ class CubicBezierSpline(p0: Vector2, p1: Vector2, p2: Vector2, p3: Vector2) : Sp
     }
 
     // B(t) = (1 - t)^3P0 + 3(1 - t)^2tP1 + 3(1 - t)t^2P2 + t^3P3
-    public override fun getPoint(t: Double): Vector2 {
+    public override fun getPoint(t: Double): Vector2d {
         val px =
-            Math.pow(1 - t, 3.0) * p0.x +
-            3 * Math.pow(1 - t, 2.0) * t * p1.x +
-            3 * (1 - t) * Math.pow(t, 2.0) * p2.x +
-            Math.pow(t, 3.0) * p3.x
+            p0.x * Math.pow(1 - t, 3.0) +
+            p1.x * 3 * Math.pow(1 - t, 2.0) * t +
+            p2.x * 3 * (1 - t) * Math.pow(t, 2.0) +
+            p3.x * Math.pow(t, 3.0)
 
         val py =
-            Math.pow(1 - t, 3.0) * p0.y +
-            3 * Math.pow(1 - t, 2.0) * t * p1.y +
-            3 * (1 - t) * Math.pow(t, 2.0) * p2.y +
-            Math.pow(t, 3.0) * p3.y
+            p0.y * Math.pow(1 - t, 3.0) +
+            p1.y * 3 * Math.pow(1 - t, 2.0) * t +
+            p2.y * 3 * (1 - t) * Math.pow(t, 2.0) +
+            p3.y * Math.pow(t, 3.0)
 
-        return Vector2(px, py)
+        return Vector2d(px, py)
     }
 
     // B'(t) = 3(1- t)^2(P1 - P0) + 6(1 - t)t(P2 - P1) + 3t^2(P3 - P2)
     public override fun getHeading(t: Double): Rotation2d {
-        val px = (1 - t) * (1 - t) * (p1.x - p0.x) + 2 * (1 - t) * t * (p2.x - p1.x) + t * t * (p3.x - p2.x)
-        val py = (1 - t) * (1 - t) * (p1.y - p0.y) + 2 * (1 - t) * t * (p2.y - p1.y) + t * t * (p3.y - p2.y)
-        return Rotation2d(px, py, true)
+        val px = (p1.x - p0.x) * (1 - t) * (1 - t) + (p2.x - p1.x) * 2 * (1 - t) * t + (p3.x - p2.x) * t * t
+        val py = (p1.y - p0.y) * (1 - t) * (1 - t) + (p2.y - p1.y) * 2 * (1 - t) * t + (p3.y - p2.y) * t * t
+        return Rotation2d(px.value, py.value, true)
     }
 
     override fun getCurvature(t: Double): Double = 0.0
-    override fun getDCurvature(t: Double) = 0.0
-    override fun getVelocity(t: Double) = 0.0
+    override fun getDCurvature(t: Double): Double = 0.0
+    override fun getVelocity(t: Double): Double = 0.0
 }
--- src/main/kotlin/org/team5419/fault/math/splines/InterlopedCubicBezierSpline.kt
+++ src/main/kotlin/org/team5419/fault/math/splines/InterlopedCubicBezierSpline.kt
@@ -1,35 +1,35 @@
 package org.team5419.fault.math.splines
 
-import org.team5419.fault.math.splines.Spline
-import org.team5419.fault.math.splines.CubicBezierSpline
-import org.team5419.fault.math.geometry.Vector2
+import org.team5419.fault.math.geometry.Vector2d
 import org.team5419.fault.math.geometry.Rotation2d
 import org.team5419.fault.util.Oof
 
-
-public class InterlopedCubicBezierSpline (vararg splines: CubicBezierSpline): Spline() {
+public class InterlopedCubicBezierSpline(vararg points: Vector2d) : Spline() {
     private val splines: Array<CubicBezierSpline>
-    private val points: Array<Vector2>
+    private val points: Array<out Vector2d>
     private val numSplines: Int
         get() = this.splines.size
 
-
-    constructor(vararg points){
-        if(points.count() % 3 != 1) throw Oof("Inncorrect number of points")
+    init {
+        if (points.count() % 3 != 1) throw Oof("Inncorrect number of points")
         this.points = points
-    }
-    init{
-        this.splines = splines
-        //TODO: add rotate to interlope
+        this.splines = Array<CubicBezierSpline>((points.count() - 1) / 3) {
+            i -> CubicBezierSpline(
+                points[i * 3],
+                points[i * 3 + 1],
+                points[i * 3 + 2],
+                points[i * 3 + 3]
+            )
+        }
     }
 
     fun getSpline(t: Double): CubicBezierSpline = this.splines[(t * this.numSplines).toInt()]
+    fun getReletiveT(t: Double): Double = (t * this.numSplines) - (t * this.numSplines).toInt()
 
-    fun getT(t: Double): Double = (t * this.numSplines) - (t * this.numSplines).toInt()
+    override fun getPoint(t: Double): Vector2d = this.getSpline(t).getPoint(this.getReletiveT(t))
+    override fun getHeading(t: Double): Rotation2d = this.getSpline(t).getHeading(this.getReletiveT(t))
+    override fun getVelocity(t: Double): Double = this.getSpline(t).getVelocity(this.getReletiveT(t))
 
-    override fun getPoint(t: Double): Vector2 = this.getSpline(t).getPoint(this.getT(t))
-    override fun getHeading(t: Double): Vector2 = this.getSpline(t).getHeading(this.getT(t))
-    override fun getCurvature(t: Double): Vector2 = this.getSpline(t).getCurvature(this.getT(t))
-
-
+    override fun getCurvature(t: Double): Double = this.getSpline(t).getCurvature(this.getReletiveT(t))
+    override fun getDCurvature(t: Double): Double = this.getSpline(t).getDCurvature(this.getReletiveT(t))
 }
